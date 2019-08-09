9d9b0886b2dabf05e07032e1bc051e9a456e5edb
Mason Schechter <masonschechter@gmail.com>
2019-08-08 19:48:50 -0500

    ready 4 TACC?

--- fasta_tools/parseUniparcChunks.py
+++ fasta_tools/parseUniparcChunks.py
@@ -1,14 +1,51 @@
-'''>UPI0000000011 status=active    MVDAITVLTAIGITVLMLLMVISGAAMIVKELNPNDIFTMQSLKFNRAVTIFKYIGLFIY    IPGTIILYATYVKSLLMKS \n'''
+'''>UPI0000000011 status=active    MVDAITVLTAIGITVLMLLMVISGAAMIVKELNPNDIFTMQSLKFNRAVTIFKYIGLFIY    IPGTIILYATYVKSLLMKS \n
+uniparc_active_chunk_#.fasta 2819 total'''
 
-import multiprocessing
+import multiprocessing as mp
 import sqlite3
 import sys
 
-with open('uniparc_active_chunk_1.fasta', 'r') as file:
-	for line in file:
-		# print(line)
-		uniparcID = line.split(' ')[0][1:]
-		status = line.split('\t')[0].split(' ')[1].split('=')[1]
-		sequence = ''
-		for _ in line.split('\t')[1:-1]:
-			sequence += _
+statusDict = {"active":1, "inactive":0}
+con = sqlite3.connect('ProteinSequences.db')
+
+start = int(sys.argv[1])
+stop = int(sys.argv[2])
+fileBase = 'uniparc_active_chunk_'
+fileNames = []
+
+
+for i in range(start,stop+1):
+    fileNames.append(f'{fileBase}{i}.fasta')
+
+def createTable():
+    db = con.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS Uniparc (UniparcID, Status, Sequence)")
+
+
+def parseFile(fileName):
+    data = []
+    with open(fileName, 'r') as file:
+        for line in file:
+            uniparcID = line.split(' ')[0][1:]
+            status = statusDict[line.split('\t')[0].split(' ')[1].split('=')[1]]
+            sequence = ''
+            for _ in line.split('\t')[1:-1]:
+                sequence += _
+            data.append((uniparcID, status, sequence))
+    return data
+
+if __name__ == '__main__':
+    createTable()
+    fastaInserted = 0
+    
+    physicalCores = int(mp.cpu_count()/2)
+    p = mp.Pool(physicalCores)
+    multiResults = p.imap(parseFile, fileNames)
+
+    for result in multiResults:
+        if not len(result) % 100000:
+            db = con.cursor()
+            db.executemany("INSERT INTO TestTable (UniparcID, Status, Sequence) VALUES (?,?,?)", result)
+            con.commit()
+            fastaInserted += 100000
+            print(f'Inserted {fastaInserted} entries into db')
