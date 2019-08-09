16f93811a2e958c6016a6570a19d77813439c0ab
Mason Schechter <masonschechter@gmail.com>
2019-08-09 10:57:05 -0500

    1.0?

---
+++ fasta_tools/make_cmd.py
@@ -0,0 +1,11 @@
+numFiles = 2819
+batchSize = 24
+remainder = numFiles % batchSize
+highestMultiple = (numFiles - remainder)//batchSize
+
+with open('parseUniparc.cmd', 'w') as file:
+    for i in range(highestMultiple):
+        start = i*batchSize+1
+        stop = (i+1)*batchSize
+        file.write(f"python3 parseUniparcChunks.py {start} {stop}\n")
+    file.write(f"python3 parseUniparcChunks.py {highestMultiple*batchSize+1} {numFiles}\n")
