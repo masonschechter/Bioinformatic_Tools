228464b32cfc3cbf8545e36cc540a605ae4d385d
Mason Schechter <masonschechter@gmail.com>
2019-08-12 17:53:18 -0500

    finished uniprot parser?

--- fasta_tools/parseUniprotChunks.py
+++ fasta_tools/parseUniprotChunks.py
@@ -25,7 +25,7 @@ for i in range(start,stop+1):
 def createTable():
     db = con.cursor()
     db.execute('''CREATE TABLE IF NOT EXISTS UniprotSP (
-                UniprotAccession TEXT PRIMARY KEY, 
+                UniprotID TEXT PRIMARY KEY, 
                 Source TEXT, 
                 EntryName TEXT, 
                 Annotation TEXT, 
@@ -35,4 +35,50 @@ def createTable():
                 ProteinExistence TEXT, 
                 SequenceVersion TEXT, 
                 Sequence TEXT)''')
+
+def fastaRegex(header):
+    processedHeader = header.split(' ')[0].split('|')
+    uniprotID = processedHeader[1]
+    source = processedHeader[0][1:]
+    entryName = processedHeader[2]
+    annotation = re.search(annotationRegex, header)[0] if re.search(annotationRegex, header) else 'NULL'
+    organism = re.search(organismRegex, header)[0] if re.search(organismRegex, header) else 'NULL'
+    organismID = re.search(organismIDRegex, header)[0] if re.search(organismIDRegex, header) else 'NULL'
+    geneName = re.search(geneNameRegex, header)[0] if re.search(geneNameRegex, header) else 'NULL'
+    proteinExistence = re.search(proteinExistenceRegex, header)[0] if re.search(proteinExistenceRegex, header) else 'NULL'
+    sequenceVersion = re.search(sequenceVersionRegex, header)[0] if re.search(sequenceVersionRegex, header) else 'NULL'
+
+    return (uniprotID, source, entryName, annotation, organism, organismID, geneName, proteinExistence, sequenceVersion)
+
+def parseFile(fileName):
+    data = []
+    with open(fileName, 'r') as file:
+        for line in file:
+            header = line.split('\t')[0]
+            seqList = line.split('\t')[1:-1]
+            sequence = ''.join(seqList)
+            uniprotID, source, entryName, annotation, organism, organismID, geneName, proteinExistence, sequenceVersion = fastaRegex(header)
+            data.append((uniprotID, source, entryName, annotation, organism, organismID, geneName, proteinExistence, sequenceVersion, sequence))
+    return data
+
+if __name__ == '__main__':
+    createTable()
+    fastaInserted = 0
+
+    physicalCores = int(mp.cpu_count()/2)
+    p = mp.Pool(physicalCores)
+    multiResults = p.imap(parseFile, fileNames)
+
+    for result in multiResults:
+        if not len(result) % 100000:
+            db = con.cursor()
+            db.executemany('''INSERT INTO UniprotSP 
+                (UniprotID, Source, EntryName, Annotation, Organism, OrganismID, GeneName, ProteinExistence, SequenceVersion, Sequence) 
+                VALUES (?,?,?)''', result)
+            con.commit()
+            fastaInserted += 100000
+            print(f'Inserted {fastaInserted} entries into db')
+        else:
+            print(f"we dun goofed.")
+    print(f"Finished parsing chunks {start} through {stop}\n")
 
